8d21ce29c1710aca1936c56f52ce6fa698220086
Thiago <thiagocrrabelo@gmail.com>
2024-02-04 03:13:38 -0300

    Update: Task controller

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskDTO } from './task.dto';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly categoryService: TaskService) {}
+
+  @Post()
+  async create(@Body() data: TaskDTO) {
+    return this.categoryService.create(data);
+  }
+}
