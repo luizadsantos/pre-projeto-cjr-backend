af7bb33d45270f6bbc52d1b9a04704baf2903a9e
Thiago <thiagocrrabelo@gmail.com>
2024-02-04 07:03:37 -0300

    Update: Controller task show routes

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService, taskResponses } from './task.service';
 import { TaskDTO } from './task.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -16,5 +16,28 @@ export class TaskController {
   @Post()
   async create(@Body() data: TaskDTO) {
     return this.taskService.create(data);
+  }
+
+  @ApiOperation({ summary: 'Show all tasks' })
+  @ApiResponse({ status: 200, description: taskResponses[200] })
+  @Get()
+  async showAll() {
+    return await this.taskService.showAll();
+  }
+
+  @ApiOperation({ summary: 'Show an specified task by id' })
+  @ApiResponse({ status: 200, description: taskResponses[200] })
+  @ApiResponse({ status: 404, description: taskResponses[404] })
+  @Get(':id')
+  async showById(@Param('id') id: string) {
+    return await this.taskService.showById(id);
+  }
+
+  @ApiOperation({ summary: 'Show an specified task by name' })
+  @ApiResponse({ status: 200, description: taskResponses[200] })
+  @ApiResponse({ status: 404, description: taskResponses[404] })
+  @Get('name/:name')
+  async showByName(@Param('name') name: string) {
+    return await this.taskService.showByName(name);
   }
 }
