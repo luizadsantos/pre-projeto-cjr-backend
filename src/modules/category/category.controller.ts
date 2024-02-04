f40cd00b0dc68006bca3c3ad35920864f4a0145f
Thiago <thiagocrrabelo@gmail.com>
2024-02-04 04:42:34 -0300

    Update: Controller category Swagger

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,11 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import { CategoryService, categoryResponses } from './category.service';
 import { CategoryDTO } from './category.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Categories routes')
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @ApiOperation({ summary: 'Create a new category' })
+  @ApiResponse({ status: 200, description: categoryResponses[200] })
+  @ApiResponse({ status: 409, description: categoryResponses[409] })
   @Post()
   async create(@Body() data: CategoryDTO) {
     return this.categoryService.create(data);
