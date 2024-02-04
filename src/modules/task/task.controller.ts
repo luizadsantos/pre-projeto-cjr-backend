da4705c518894efebdc62772745d68ad28848d7d
Thiago <thiagocrrabelo@gmail.com>
2024-02-04 04:42:58 -0300

    Update: Controller task Swagger

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { TaskService } from './task.service';
+import { TaskService, taskResponses } from './task.service';
 import { TaskDTO } from './task.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tasks routes')
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiResponse({ status: 200, description: taskResponses[200] })
+  @ApiResponse({ status: 409, description: taskResponses[409] })
+  @ApiResponse({ status: 404, description: taskResponses[404] })
+  @ApiResponse({ status: 400, description: taskResponses[400] })
   @Post()
   async create(@Body() data: TaskDTO) {
     return this.taskService.create(data);
