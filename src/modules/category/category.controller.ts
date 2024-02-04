1946db2a4eb81a724c43a2887f707643020c5b9f
Thiago <thiagocrrabelo@gmail.com>
2024-02-04 07:03:10 -0300

    Update: Controller category show routes

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CategoryService, categoryResponses } from './category.service';
 import { CategoryDTO } from './category.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -13,6 +13,29 @@ export class CategoryController {
   @ApiResponse({ status: 409, description: categoryResponses[409] })
   @Post()
   async create(@Body() data: CategoryDTO) {
-    return this.categoryService.create(data);
+    return await this.categoryService.create(data);
+  }
+
+  @ApiOperation({ summary: 'Show all categories' })
+  @ApiResponse({ status: 200, description: categoryResponses[200] })
+  @Get()
+  async showAll() {
+    return await this.categoryService.showAll();
+  }
+
+  @ApiOperation({ summary: 'Show an specified category by id' })
+  @ApiResponse({ status: 200, description: categoryResponses[200] })
+  @ApiResponse({ status: 404, description: categoryResponses[404] })
+  @Get(':id')
+  async showById(@Param('id') id: string) {
+    return await this.categoryService.showById(id);
+  }
+
+  @ApiOperation({ summary: 'Show an specified category by name' })
+  @ApiResponse({ status: 200, description: categoryResponses[200] })
+  @ApiResponse({ status: 404, description: categoryResponses[404] })
+  @Get('name/:name')
+  async showByName(@Param('name') name: string) {
+    return await this.categoryService.showByName(name);
   }
 }
